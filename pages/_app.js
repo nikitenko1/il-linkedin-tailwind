d55b0fc7b51879927e5b50bcdcc4d51dc2bc0053
nikitenko <nikitenko.max@gmail.com>
2022-01-27 17:32:59 +0200

    05-UI-sidebar

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,17 @@
 import '../styles/globals.css';
 import { ThemeProvider } from 'next-themes';
+import { SessionProvider } from 'next-auth/react';
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <ThemeProvider attribute="class">
-      <Component {...pageProps} />
-    </ThemeProvider>
+    // Version 4 makes using the SessionProvider mandatory.
+    // `session` comes from `getServerSideProps` or `getInitialProps`.
+    // Avoids flickering/session loading on first load.
+    <SessionProvider session={session}>
+      <ThemeProvider attribute="class">
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </SessionProvider>
   );
 }
 
